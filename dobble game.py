0d9376da4d11633fe5cc12edc2661bfbdcbe5ffa
Raj <rajkar921@gmail.com>
2021-04-16 18:57:13 +0530

    Add new words

--- dobble game.py
+++ dobble game.py
@@ -1,13 +1,22 @@
 words = ['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he', 'was', 'for', 'on', 'are', 'with', 'as', 'I', 'his', 'they', 'be', 'at', 'one', 'have', 'this', 'from', 'or', 'had', 'by', 'not', 'word', 'but', 'what', 'some', 'we', 'can', 'out', 'other', 'were', 'all', 'there', 'when', 'up', 'use', 'your', 'how', 'said', 'an', 'each', 'she', 'which', 'do', 'their', 'time', 'if', 'will', 'way', 'about', 'many', 'then', 'them', 'write', 'would', 'like', 'so', 'these', 'her', 'long', 'make', 'thing', 'see', 'him', 'two', 'has', 'look', 'more', 'day', 'could', 'go', 'come', 'did', 'number', 'sound', 'no', 'most', 'people', 'my', 'over', 'know', 'water', 'than', 'call', 'first', 'who', 'may', 'down', 'side', 'been', 'now', 'find', 'any', 'new', 'work', 'part', 'take',
          'get', 'place', 'made', 'live', 'where', 'after', 'back', 'little', 'only', 'round', 'man', 'year', 'came', 'show', 'every', 'good', 'me', 'give', 'our', 'under', 'name', 'very', 'through', 'just', 'form', 'sentence', 'great', 'think', 'say', 'help', 'low', 'line', 'differ', 'turn', 'cause', 'much', 'mean', 'before', 'move', 'right', 'boy', 'old', 'too', 'same', 'tell', 'does', 'set', 'three', 'want', 'air', 'well', 'also', 'play', 'small', 'end', 'put', 'home', 'read', 'hand', 'port', 'large', 'spell', 'add', 'even', 'land', 'here', 'must', 'big', 'high', 'such', 'follow', 'act', 'why', 'ask', 'men', 'change', 'went', 'light', 'kind', 'off', 'need', 'house', 'picture', 'try', 'us', 'again', 'animal', 'point', 'mother', 'world', 'near', 'build', 'self', 'earth', 'father']
 
+
+words = ['goon', 'loser',  'criminal', 'carbon',  'collection',  'helpless',  'crisis', 'adventure', 'bloodsport',
+         'pulse', 'eternal', 'swarm', 'illegal', 'hospital', 'zipper', 'devices', 'atonement', 'port', 'anxiety',
+         'allotment', 'southern', 'beyond', 'burn', 'concussion', 'branch', 'orb', 'firecracker', 'herd',
+                      'graveyard', 'homeless', 'acidic', 'liqueur', 'crasher', 'piece', 'wreckage', 'settler', 'signal',
+                      'circuitry', 'boulevard', 'derelict', 'pimp', 'chart', 'glacial', 'bloodstain', 'formal', 'halloweenedge',
+                      'commando', 'drugstore', 'raster', 'flowers', 'fuzzy', 'founder', 'growl', 'concave', 'fizz']
+
+
 print("Find the similarity --> ")
-print(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'], ['get', 'place', 'made', 'and', 'where', 'after'])
+print(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'],
+      ['get', 'place', 'made', 'and', 'where', 'after'])
 user_word = input("Enter the similar word -> ")
 
 if user_word == similar_word:
-	print("CORRECTTTTTTT !!!!")
+    print("CORRECTTTTTTT !!!!")
 else:
-	# print("The first letter stats with a") Hint
-	print(f"The correct word is {similar_word}")
-	
+    # print("The first letter stats with a") Hint
+    print(f"The correct word is {similar_word}")
